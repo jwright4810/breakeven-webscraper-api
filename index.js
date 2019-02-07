982826fab266034da30e14135b8f70e562f619e5
Jarett Wright <jwtech8313@gmail.comgit confit --list>
2019-02-06 21:20:59 -0600

    inital commits

--- index.js
+++ index.js
@@ -13,54 +13,46 @@ const app = express();
 app.use(bodyParser.json()); 
 app.use(cors()); 
 
-let $ = jQuery = require('jquery')(window); 
+ 
 
 let futPrices = {};
 
+//iife uses nighmarejs to scrape cme website and return future prices into the futPrices object
+(function () {
+    nightmare.goto('https://www.cmegroup.com/trading/agricultural/livestock/feeder-cattle_quotes_globex.html')
+    .wait(2000)
+    .evaluate(function(){
 
+        const futObj = {}; 
+        
+        let rows = document.getElementById('quotesFuturesProductTable1').rows;
+        
+        for(let i = 1; i < rows.length; i++) {
+           futObj[rows[i].cells[1].innerText] = rows[i].cells[3].innerText;  
+        }
+        
+        return futObj; 
+    })
+    .end()
+    .then(result => {
+        for(let props in result) {
+            futPrices[props] = result[props]; 
+        }
+        console.log('scraper executed')
+    })
+    .catch(err => console.log(err))
+})(); 
 
-
-app.get('/', (req, res) => {
-    (function () {
-        nightmare.goto('https://www.cmegroup.com/trading/agricultural/livestock/feeder-cattle_quotes_globex.html')
-        .wait(2000)
-        .evaluate(function(){
-    
-            const futObj = {}; 
-            
-            let rows = document.getElementById('quotesFuturesProductTable1').rows;
-            
-            for(let i = 1; i < rows.length; i++) {
-               futObj[rows[i].cells[1].innerText] = rows[i].cells[3].innerText;  
-            }
-            
-            return futObj; 
-        })
-        .end()
-        .then(result => {
-            for(let props in result) {
-                futPrices[props] = result[props]; 
-            }
-            res.json('scraper executed')
-        })
-        .catch(err => console.log(err))
-    })(); 
-})  
-
-
+app.get('/', (req, res) => {res.send('it is working!')})
 app.get('/setprices', (req, res) => {
     res.json(futPrices)
 })
 
-
-
-app.listen(process.env.PORT || 3000, () => {
+app.listen(3000, () => {
     console.log('app is running on port 3000'); 
 })
                      
-// app.listen( 3001, () => {
-//     console.log('app is running on port 3000'); 
-// })
+
     
 
 
